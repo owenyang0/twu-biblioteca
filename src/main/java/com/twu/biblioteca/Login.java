e0164dd8028f91c62a3e2fc5499cc4a7106163e6
OwenYang <usay@outlook.com>
2014-10-07 20:26:48 +0800

    auth: add login auth

--- src/main/java/com/twu/biblioteca/Login.java
+++ src/main/java/com/twu/biblioteca/Login.java
@@ -1,6 +1,9 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.Modal.User;
+
 import java.io.PrintStream;
+import java.util.List;
 
 /**
  * Created by songyang on 10/5/14.
@@ -8,9 +11,27 @@ import java.io.PrintStream;
 public class Login {
     private PrintStream printStream;
     private UserDao userDao;
+    private boolean authorized = false;
+    private List<User> users;
 
     public Login(PrintStream printStream, UserDao userDao) {
         this.printStream = printStream;
         this.userDao = userDao;
+        this.users = userDao.getUsers();
+    }
+
+    public boolean isLogin() {
+        return authorized;
+    }
+
+    public boolean author(String libraryNumber, String password) {
+        for (User user : users) {
+            if (user.getLibraryNumber().equals(libraryNumber) && user.getPassword().equals(password)) {
+                authorized = true;
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/test/java/com/twu/biblioteca/LoginTest.java
+++ src/test/java/com/twu/biblioteca/LoginTest.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.PrintStream;
 
@@ -17,5 +18,28 @@ public class LoginTest {
         printStream = mock(PrintStream.class);
         userDao = new UserDao();
         login = new Login(printStream, userDao);
+    }
+
+    @Test
+    public void should_return_false_when_first_time_get_status() throws Exception {
+        assertFalse(login.isLogin());
+    }
+
+    @Test
+    public void should_return_true_after_correct_auth() throws Exception {
+        String validLibraryNO = "111-2222";
+        String validPassword = "123";
+
+        assertTrue(login.author(validLibraryNO, validPassword));
+        assertTrue(login.isLogin());
+    }
+
+    @Test
+    public void should_return_false_when_login_with_invalid_info() throws Exception {
+        String invalidLibraryNO = "111-2222-0";
+        String invalidPassword = "123";
+
+        assertFalse(login.author(invalidLibraryNO, invalidPassword));
+        assertFalse(login.isLogin());
     }
 }
